9053c57ee89fbfae2270b0446117ab2394f94ab6
Philip Raab <philip@cathedral.co.za>
2025-03-14 15:55:42 +0200

    example simplify #2

--- examples/menu.php
+++ examples/menu.php
@@ -22,20 +22,17 @@ while (true) {
 	$choice = \Inane\Cli\Cli::menu($menu, null, 'Choose an example');
 	\Inane\Cli\Cli::line();
 
+	$queue = [$choice => ''];
+
 	if ($choice == '_quit') {
 		break;
+	} elseif ($choice == '_all') {
+		$queue = array_slice($menu, 0, -2);
 	}
 
-	if (!str_starts_with($choice, '_')) {
-		include "{$choice}.php";
+	foreach($queue as $file => $description) {
+		$title->line("\n\t\t\t\t$description\n");
+		include "{$file}.php";
 		\Inane\Cli\Cli::line();
-	} elseif ($choice == '_all') {
-		foreach($menu as $file => $description) {
-			if (!str_starts_with($file, '_')) {
-				$title->line("\n\t\t\t\t$description\n");
-				include "{$file}.php";
-				\Inane\Cli\Cli::line();
-			}
-		}
 	}
 }
