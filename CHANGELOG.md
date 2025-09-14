f1fc8c5b76669da9f21c393ebb15b98ddc66d347
Philip Michael Raab <philip@cathedral.co.za>
2025-09-14 19:18:59 +0200

    Console Script Handling
    * new: `Console` Three classes that handle a directory of cli script files. Running them as templates an making range of methods available to them.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,56 +1,91 @@
-# Changelog: Cli
+Version: $Id$ ($Date$)
 
-> version: $Id$ ($Date$)
+# History
 
-## History
+## 0.17.0-dev (2025 Xxx xx)
 
-### 0.16.0 (2025 Apr 25)
+- new: `Console` Three classes that handle a directory of cli script
+  files. Running them as templates an making range of methods available
+  to them.
 
- - menu  : now takes int and string values for menu index
- - update: `Pencil::pad` now has a padString argument
- - update: `Notify` improved parametar types
+## 0.12.1 (2022 Aug 11)
 
-### 0.15.0 (2025 Mar 14)
+- fix: `TextTable` `divider` ⇒ uses full column width
 
- - fix: `Lexer::unshift` return type
- - fix: `Arguments::parseFlag` not incrementing stackable flags
- - other minor updates and fixes (mainly to code comments & variable typing)
- - switch from `posix_isatty` to `stream_isatty` for better compatibility
- - windows compatibility improvements
- - update return & param types
- - update code docs
- - simplify inter-class function calls
- - improved File writing
+- fix: `TextTable` coloured string cell width
 
-### 0.14.1 (2023 May 03)
+- fix: `Colors::length` error
 
- - new: `Cli::shellEnv` return enum Environment type: none, interactive, non-interactive
- - update: minor tweaks, improvements and updates
+## 0.13.0 (2022 Oct 10)
 
-### 0.14.0 (2022 Dec 21)
+- fix: STD(OUT/IN/ERR) undefined
 
- - new: `Pencil::format` returns colour string without outputting it
- - new: `Pencil::input` reads terminal input
- - new: `Pencil` cache original text for reversing it in certain situations
- - new: `Colour::Default` system colour
- - fix: `Arguments::getOption` return type
- - fix: various little updates and fixes
- - update: `Arguments` implements `\Inane\Stdlib\Converters\JSONable`
- - update: `Arguments::toJSON` added $pretty option to format json
- - update: `HelpScreen` fixes for *php 8.1*
- - update: `\Inane\Stdlib\Json` start to replace `json_*`
- - update: **inanepain/stdlib** requirement bumped to **0.4.0**
+- new: `Cli::isCliServer` Is PHP built-in server
 
-### 0.13.0 (2022 Oct 10)
+- new: `Pencil` stationary tools
 
- - fix: STD(OUT/IN/ERR) undefined
- - new: `Cli::isCliServer` Is PHP built-in server
- - new: `Pencil` stationary tools
- - upd: `Cli::input` new optional arg `$default` returned if non-interactive shell
- - many minor tweaks, updates and fixes
+- upd: `Cli::input` new optional arg `$default` returned if
+  non-interactive shell
 
-### 0.12.1 (2022 Aug 11)
+- many minor tweaks, updates and fixes
 
- - fix: `TextTable` `divider` => uses full column width
- - fix: `TextTable` coloured string cell width
- - fix: `Colors::length` error
+## 0.14.0 (2022 Dec 21)
+
+- new: `Pencil::format` returns colour string without outputting it
+
+- new: `Pencil::input` reads terminal input
+
+- new: `Pencil` cache original text for reversing it in certain
+  situations
+
+- new: `Colour::Default` system colour
+
+- fix: `Arguments::getOption` return type
+
+- fix: various little updates and fixes
+
+- update: `Arguments` implements `\Inane\Stdlib\Converters\JSONable`
+
+- update: `Arguments::toJSON` added $pretty option to format json
+
+- update: `HelpScreen` fixes for **php 8.1**
+
+- update: `\Inane\Stdlib\Json` start to replace `json_*`
+
+- update: **inanepain/stdlib** requirement bumped to **0.4.0**
+
+## 0.14.1 (2023 May 03)
+
+- new: `Cli::shellEnv` return enum Environment type: none, interactive,
+  non-interactive
+
+- update: minor tweaks, improvements and updates
+
+## 0.15.0 (2025 Mar 14)
+
+- fix: `Lexer::unshift` return type
+
+- fix: `Arguments::parseFlag` not incrementing stackable flags
+
+- other minor updates and fixes (mainly to code comments & variable
+  typing)
+
+- switch from `posix_isatty` to `stream_isatty` for better compatibility
+
+- windows compatibility improvements
+
+- update return & param types
+
+- update code docs
+
+- simplify inter-class function calls
+
+- improved File writing
+
+## 0.16.0 (2025 Apr 25)
+
+- menu : now takes int and string values for menu index
+
+- update: `Pencil::pad` now has a padString argument
+
+- update: `Notify` improved parametar types
---
+++ src/Console/ConsoleScriptManager.php
@@ -0,0 +1,217 @@
+<?php
+
+/**
+ * Inane: Cli
+ *
+ * Utilities to simplify working with the console.
+ *
+ * $Id$
+ * $Date$
+ *
+ * PHP version 8.4
+ *
+ * @author  James Logsdon <dwarf@girsbrain.org>
+ * @author  Philip Michael Raab<philip@cathedral.co.za>
+ * @package inanepain\cli
+ * @category cli
+ *
+ * @license UNLICENSE
+ * @license https://unlicense.org/UNLICENSE UNLICENSE
+ *
+ * _version_ $version
+ */
+
+declare(strict_types=1);
+
+namespace Inane\Cli\Console;
+
+use Inane\Config\Config;
+use Inane\File\Path;
+use Inane\Stdlib\Array\OptionsInterface;
+use Inane\Stdlib\Options;
+
+use function class_exists;
+
+use const false;
+use const true;
+
+/**
+ * Manages the execution and registration of console scripts.
+ *
+ * This class provides methods to handle console-based scripts,
+ * including their initialization, execution, and management within
+ * the CLI environment.
+ */
+class ConsoleScriptManager {
+    /**
+     * The configuration instance used by the ConsoleScriptManager.
+     *
+     * @var Config
+     */
+    protected Config $config;
+    /**
+     * The pen instance used for CLI output formatting and styling.
+     *
+     * @var CliPen
+     */
+    protected CliPen $pen;
+    /**
+     * The directory path where console scripts are stored.
+     *
+     * @var Path
+     */
+    private Path $scriptDir;
+    /**
+     * Holds the available script options for the console script manager.
+     *
+     * @var Options $scripts Collection of script options.
+     */
+    private Options $scripts;
+
+    /**
+     * Exit after all enabled cli includes have run.
+     * 
+     * @see falseUntilTrue
+     * 
+     * @var bool
+     */
+    public bool $exitAfterLastInclude {
+        get => isset($this->exitAfterLastInclude) ? $this->exitAfterLastInclude : false;
+        set => $this->exitAfterLastInclude = isset($this->exitAfterLastInclude) == true ? ($this->exitAfterLastInclude ? true : $value) : $value;
+    }
+
+    /**
+     * True if at least one script ran.
+     * 
+     * @see falseUntilTrue
+     * 
+     * @var bool
+     */
+    public bool $scriptHasRun {
+        get => isset($this->scriptHasRun) ? $this->scriptHasRun : false;
+        set => $this->scriptHasRun = isset($this->scriptHasRun) == true ? ($this->scriptHasRun ? true : $value) : $value;
+    }
+
+    /**
+     * ConsoleScriptManager constructor.
+     *
+     * Initializes the ConsoleScriptManager with the provided configuration options.
+     *
+     * @param OptionsInterface $config Configuration options for the script manager.
+     */
+    public function __construct(OptionsInterface $config) {
+        $this->configure($config);
+
+        $this->bootstrap();
+    }
+
+    /**
+     * Configures the console script manager with the provided options.
+     *
+     * @param OptionsInterface $config The configuration options to apply.
+     *
+     * @return void
+     */
+    protected function configure(OptionsInterface $config): void {
+        // We store the `console` section of $config
+        // First we load the default config. Then use modify to update it with user config.
+        $this->config = new Config([
+            // 'script-dir' => 'src-cli',
+            'script' => [
+                'path' => 'src-cli',
+                'notice' => [
+                    'expired' => false,
+                ],
+            ],
+            'dumper' => [
+                'buffer' => true,
+                'runkit7' => false,
+            ],
+        ])->modify($config->console)->lock();
+
+        if (class_exists('\Inane\Dumper\Dumper')) {
+            \Inane\Dumper\Dumper::$bufferOutput = $this->config->dumper->buffer;
+            \Inane\Dumper\Dumper::$showRunkit7SupportMessage = $this->config->dumper->runkit7;
+        }
+
+        $this->scriptDir = new Path($this->config->script->path);
+        $this->scripts = new Options();
+    }
+
+    /**
+     * Initializes and sets up the necessary environment or dependencies
+     * required before executing the main logic of the script manager.
+     *
+     * This method is intended to be called internally to prepare the
+     * script manager for operation.
+     *
+     * @return void
+     */
+    protected function bootstrap(): void {
+        $this->pen = new CliPen();
+
+        foreach ($this->scriptDir->getFiles('*.php') ?: [] as $file) {
+            $this->scripts->set($file->getBasename('.php'), new CliScript($file, $this->config->script));
+        }
+    }
+
+    /**
+     * Executes the script manager's main logic.
+     *
+     * @return self Returns the current instance for method chaining.
+     */
+    public function run(): self {
+        /**
+         * @var CliScript $script
+         */
+        foreach ($this->scripts as $script) {
+            $this->exitAfterLastInclude = $script
+                ->run()
+                ->end()
+                ->exitAfterLastInclude;
+            $this->scriptHasRun = $script->hasRun;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Logs information related to the script execution.
+     *
+     * @return false|int Returns false on failure, or an integer log identifier on success.
+     */
+    public function log(): false|int {
+        $this->pen->divider(pencil: $this->pen->red);
+
+        if ($this->scripts->count() == 0) return !((bool)$this->pen->red->line('No scripts found to run.'));
+
+        $table = new \Inane\Cli\TextTable();
+        $table->addHeader(['Script', 'Did Run']);
+
+        /**
+         * @var CliScript $script
+         */
+        foreach ($this->scripts as $name => $script)
+            $table->addRow([$name, ($script->hasRun ? '✓' : '')]);
+
+        $this->pen->default->line($table->render());
+
+        return $this->scripts->count();
+    }
+
+    /**
+     * Ends the current console script execution.
+     *
+     * @param bool $log Optional. Whether to log the end of the script. Default is false.
+     * 
+     * @return void
+     */
+    public function end(bool $log = false): void {
+        if ($log && $this->scriptHasRun) $this->log();
+
+        if ($this->exitAfterLastInclude) {
+            $this->pen->red->line('Exiting after last include.');
+            exit();
+        }
+    }
+}
