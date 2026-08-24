120793f9b2b00ee02814a6269e275414fb433d63
Philip Michael Raab <philip@cathedral.co.za>
2026-08-24 20:48:33 +0200

    * new: `Colour::text` for creating coloured text directly from the enum.

--- examples/pencil.php
+++ examples/pencil.php
@@ -1,13 +1,13 @@
 <?php
 
-use \Inane\Cli\Pencil;
-use \Inane\Cli\Pencil\Colour;
-use \Inane\Cli\Pencil\Style;
+use Inane\Cli\Pencil;
+use Inane\Cli\Pencil\Colour;
+use Inane\Cli\Pencil\Style;
 
 require_once 'common.php';
 
 /*
-Pencils can be use by method or echo like functions or mixing both.
+Pencils can be used by method or echo like functions or mixing both.
 */
 
 #region Pencils
@@ -20,19 +20,20 @@ $cyanBold = new Pencil(Colour::Cyan, Style::Bold);
 $blueUnderline = new Pencil(Colour::Blue, Style::Underline);
 #endregion Pencils
 
-$green->line('');
-// Using the methods to write `reset` it defaults to reset at the end of text.
+$green->line();
+// Using the methods to write `reset`, it defaults to reset at the end of text.
 $green->line('Green');
-$default->line('');
+$default->line();
 $greenBlink->line('Green');
 // Using echo, reset must be manually called.
 //echo "{$green}green", Pencil::reset(), "\n";
 $default->line('Default');
+$default->line(Colour::Red->text('Red Text') . ': Default');
 $defaultBlink->line('Default');
 $green->out('green ');
 $greenBlinkBGRed->line('blink green with bg-red');
 //echo "{$green}green {$greenBlinkBGRed}bg-green", Pencil::reset(), "\n";
-$default->line('');
+$default->line();
 $default->line('Default');
 // Here a pencil method is used and another pencil added to the string to underline that section.
 $cyanBold->line("Name: {$blueUnderline}Pencil");
--- src/Pencil/Colour.php
+++ src/Pencil/Colour.php
@@ -25,8 +25,10 @@ declare(strict_types=1);
 
 namespace Inane\Cli\Pencil;
 
+use Inane\Cli\Pencil;
 use Inane\Stdlib\Enum\CoreEnumInterface;
 use Inane\Stdlib\Enum\CoreEnumTrait;
+use InvalidArgumentException;
 
 /**
  * Colour
@@ -38,14 +40,56 @@ enum Colour: int implements CoreEnumInterface {
      * Default uses the environments colour
      */
     case Default = -1;
-    case Black   = 0;
-    case Red     = 1;
-    case Green   = 2;
-    case Yellow  = 3;
-    case Blue    = 4;
-    case Purple  = 5;
-    case Cyan    = 6;
-    case White   = 7;
+    /**
+     * Black represents the colour black.
+     */
+    case Black = 0;
+    /**
+     * Red represents the colour red with a value of 1.
+     */
+    case Red   = 1;
+    /**
+     * Represents the colour green.
+     */
+    case Green = 2;
+    /**
+     * Represents the colour yellow.
+     */
+    case Yellow = 3;
+    /**
+     * Represents the colour blue.
+     */
+    case Blue = 4;
+    /**
+     * Purple represents the colour purple with a value of 5.
+     */
+    case Purple = 5;
+    /**
+     * Represents the cyan colour.
+     */
+    case Cyan = 6;
+    /**
+     * White represents the colour white.
+     */
+    case White = 7;
 
     use CoreEnumTrait;
+
+    /**
+     * Formats and returns a text string with a specified style and colour.
+     *
+     * @param string     $text  The text to be formatted.
+     * @param Style|null $style The style to apply to the text.
+     * @param bool       $reset Whether to reset the text style and colour after formatting.
+     *
+     * @return string The formatted text string.
+     *
+     * @throws InvalidArgumentException If the style provided is not valid.
+     */
+    public function text(string $text = '', ?Style $style = null, bool $reset = true): string {
+        $colour = $this->value >= 0 ? Type::Plain->value + $this->value : 0;
+
+        $textStyle = $style === null ? '' : "$style->value;";
+        return "\033[$textStyle{$colour}m$text" . $reset ? Pencil::reset() : '';
+    }
 }
